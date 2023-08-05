0133ada78be3e77017eaf985923e373abe16f570
Saurabh Kushwaha <saurabhkushwaha697@gmail.com>
2023-08-05 22:44:25 +0530

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");+
+
+
+app.listen(5000, function () {
+  console.log(`Server started at `+ 5000);
+});
