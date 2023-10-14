d103e4b5e36c192b4e16d40712869bf95426835c
Gyaneshwar <gyaneshwar.knp.2019@gmail.com>
2023-10-14 23:57:22 +0530

    front_end_updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Menu from './components/menu/Menu'; 
-import Cart from './components/cart/Cart';
-import Login from "./components/Login/Login"
-import Signup from './components/Login/Signup';
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Orders from './components/orders/Orders';
-import Profile from './components/profile/Profile';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Cart from "./components/cart/Cart";
+import Login from "./components/Login/Login";
+import Signup from "./components/Login/Signup";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Orders from "./components/orders/Orders";
+import Profile from "./components/profile/Profile";
+import Home from "./components/home/Home";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
+        <Routes> 
+          <Route path="/" element={<Home />} />
           <Route path="menu" element={<Menu />} />
           <Route path="cart" element={<Cart />} />
           <Route path="login" element={<Login />} />
-          <Route path='signup' element = {<Signup />} />
-          <Route path='orders' element = {<Orders />} />
-          <Route path='profile' element = {<Profile />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="profile" element={<Profile />} />
         </Routes>
         <ToastContainer />
       </div>
